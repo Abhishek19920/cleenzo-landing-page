fc590ea81a559e27be604e5e31eefa639d9fe300
sunit shakya <sunit07shakya@gmail.com>
2026-06-05 17:30:52 +0530

    redirection tmp

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+// Redirect HTTPS to HTTP
+if (window.location.protocol === "https:") {
+  window.location.href = window.location.href.replace("https://", "http://");
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
