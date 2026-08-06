52703688ae03f02311a4ba84b91cc844b8e4d91d
Abhishek19920 <abhishukla465@gmail.com>
2026-08-06 20:15:55 +0530

    Show Freedom and Rakhi campaign theme from Aug 6

--- src/utils/freedomCampaign.js
+++ src/utils/freedomCampaign.js
@@ -1,7 +1,7 @@
 import { isOfferActive } from "./offerDates";
 
 /** Freedom & Independence homepage Tiranga theme (hero through process). */
-export const HOME_TIRANGA_START = "2026-08-09";
+export const HOME_TIRANGA_START = "2026-08-06";
 export const HOME_TIRANGA_END = "2026-08-30";
 
 export function isHomeTirangaThemeActive(now = new Date()) {
